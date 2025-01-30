b2c7710e955af92163d877a5705f161bc6be1ff1
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-01-30 22:23:23 +0900

    feat: 일기 필터링 기능

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,23 @@ import Notfound from './pages/Notfound';
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime,
+    createdDate: new Date("2025-01-30").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime,
+    createdDate: new Date("2025-02-01").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
   },
-]
+  {
+    id: 3,
+    createdDate: new Date("2024-12-25").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  },
+];
 
 function reducer(state, action) {
   switch(action.type) {
@@ -37,8 +43,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,61 @@
+import { useState, useContext } from 'react';
+import { DiaryStateContext } from '../App';
+
 import Header from '../components/Header'
 import Button from '../components/Button'
 import DiaryList from '../components/DiaryList';
 
+const getMonthlyData = (pivotDate, data) => {
+    // pivotDate값을 기준으로 현재 state(data)를 필터링
+    
+    const beginTime = new Date(
+        pivotDate.getFullYear(),
+        pivotDate.getMonth(),
+        1,
+        0,
+        0,
+        0,
+    ).getTime();
+    // 이달의 시작시간
+
+    const endTime = new Date(
+        pivotDate.getFullYear(),
+        pivotDate.getMonth() + 1,
+        0,
+        23,
+        59,
+        59
+    ).getTime();
+    // 이달의 종료시간
+
+    return data.filter((item) => 
+        beginTime <= item.createdDate && item.createdDate <= endTime)
+    // 이달의 일기 데이터 필터링 설정
+}
+
 const Home = () => {
+    const data = useContext(DiaryStateContext);
+    // DiaryStateContext로부터 데이터 공급
+    const [pivotDate, setPivotDate] = useState(new Date());
+    // setPivotDate 상태 생성
+
+    const monthlyData = getMonthlyData(pivotDate, data);
+    console.log(monthlyData)
+
+    const onIncreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1))
+    }
+    const onDecreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1))
+    }
+    // 달을 증가, 감소시키는 함수
+
     return (
         <div>
             <Header 
-            title={'2025년 2월'} 
-            leftchild={<Button text={"<"}/>}
-            rightchild={<Button text={">"}/>}
+            title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`} 
+            leftchild={<Button onClick={onDecreaseMonth} text={"<"}/>}
+            rightchild={<Button onClick={onIncreaseMonth} text={">"}/>}
             />
             <DiaryList/>
         </div>
