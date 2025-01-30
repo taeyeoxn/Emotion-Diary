a89068bbbb952b006608505f7e8375bb0a93c3d9
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-01-30 21:35:06 +0900

    feat: Home 페이지 퍼블리싱

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
+import Header from '../components/Header'
+import Button from '../components/Button'
+import DiaryList from '../components/DiaryList';
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Header 
+            title={'2025년 2월'} 
+            leftchild={<Button text={"<"}/>}
+            rightchild={<Button text={">"}/>}
+            />
+            <DiaryList/>
+        </div>
     )
 }
 
